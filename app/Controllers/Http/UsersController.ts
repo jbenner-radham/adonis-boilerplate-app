705ae5e556153c359d0b393ed873d92e462740f7
James Benner <james.benner@gmail.com>
2022-03-13 20:29:11 -0400

    Add create & store methods to users controller

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,11 +1,19 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
 
 export default class UsersController {
   public async index({}: HttpContextContract) {}
 
-  public async create({}: HttpContextContract) {}
+  public async create ({ view }: HttpContextContract) {
+    return view.render('users/create')
+  }
 
-  public async store({}: HttpContextContract) {}
+  public async store ({ request }: HttpContextContract) {
+    await User.create({
+      email: request.input('email'),
+      password: request.input('password'),
+    })
+  }
 
   public async show({}: HttpContextContract) {}
 
